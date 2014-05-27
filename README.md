c6031f984a359889f8afa3e31bddc170ac7731ec
jwhyte88 <jwhyte@opentable.com>
2014-05-27 14:07:04 +0100

    first commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+Fizz Buzz
+=========
+
+### [Fizz Buzz](http://en.wikipedia.org/wiki/Fizz_buzz) implementated with [TDD](http://en.wikipedia.org/wiki/Test-driven_development) (using [RSpec](http://rspec.info)).
+
+
+A simple game but also used as a coding kata. A video of my best time will be linked here. 
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,16 @@
+require 'fizzbuzz'
+
+describe 'FizzBuzz' do
+	context 'knows that a number is divisible by' do
+		it '3' do
+			expect(is_divisible_by_three?(3)).to be_true
+		end
+	end
+	context 'knows that a number is not divisible by' do
+		it '3' do
+			expect(is_divisible_by_three?(1)).not_to be_true
+		end
+	end
+end
+
+
