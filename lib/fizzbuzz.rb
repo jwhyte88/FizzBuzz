3ea29bba51984a4f7a45f182d88598483d1367b3
jwhyte88 <jwhyte@opentable.com>
2014-05-27 22:50:29 +0100

    Updated RSpec tests and FizzBuzz files

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,22 @@
-def is_divisible_by_three?(number)
+def fizzbuzz (number)
+	return 'FizzBuzz' if is_divisible_by_fifteen?(number)
+	return 'Buzz' if is_divisible_by_five?(number)
+	return 'Fizz' if is_divisible_by_three?(number)
+	number
+end
+
+def is_divisible_by_three? (number)
 	number % 3 == 0
 end
+
+def is_divisible_by_five? (number)
+	number % 5 == 0
+end
+
+def is_divisible_by_fifteen? (number)
+	number % 15 == 0
+end
+
+
+puts 'The answers to the game of FizzBuzz from 1 - 100 are below:'
+puts 1.upto(100){|number| puts fizzbuzz(number) }
